48d271f03f3d9371430c546b557c225dde6d6fc1
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-10-20 20:29:42 +0200

    themeprovider wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,15 @@ import {
 import './index.css';
 
 import App from './App';
+import StyledTheme from './Config/StyledTheme/StyledTheme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <StyledTheme>
+      <Router>
+        <App />
+      </Router>
+    </StyledTheme>
   </React.StrictMode>,
   document.getElementById('root')
 );
