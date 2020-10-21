6f23002b91e0ce9c320a881500e83439b0abc5cf
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-10-21 20:34:49 +0200

    redux planets

--- src/store/root.reducer.tsx
+++ src/store/root.reducer.tsx
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
+import planetsReducer from './planets/planets.reducer';
 
 
 const rootReducer = combineReducers({
-
-
+    planets: planetsReducer
 });
 
 export default rootReducer;
