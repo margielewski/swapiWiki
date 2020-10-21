a78d022ee74b517f48d1225570c08ac94439385f
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-10-21 14:40:48 +0200

    main

--- src/Views/Main/Main.tsx
+++ src/Views/Main/Main.tsx
@@ -2,25 +2,48 @@ import React from 'react'
 
 import {
     StyledWrapper,
-    StyledImage
+    StyledOption,
+    StyledImage,
+    StyledName
 } from './style'
 
 import starshipImg from '../../assets/starshipExample.png'
 import characterImg from '../../assets/characterExample.png'
 import planetImg from '../../assets/planetExample.png'
 
+const mainOptions = [
+    {
+        label: 'Starships',
+        img: starshipImg,
+        alt: 'star wars starship',
+    },
+    {
+        label: 'Characters',
+        img: characterImg,
+        alt: 'star wars character',
+    },
+    {
+        label: 'Planets',
+        img: planetImg,
+        alt: 'star wars planet',
+    },
+]
+
 export default function Main() {
     return (
         <StyledWrapper>
-            <StyledImage>
-                <img src={starshipImg} alt="" />
-            </StyledImage>
-            <StyledImage>
-                <img src={characterImg} alt="" />
-            </StyledImage>
-            <StyledImage>
-                <img src={planetImg} alt="" />
-            </StyledImage>
+            {
+                mainOptions.map(({ label, img, alt }) => (
+                    <StyledOption key={label}>
+                        <StyledImage>
+                            <img src={img} alt={alt} />
+                        </StyledImage>
+                        <StyledName>{label}</StyledName>
+                    </StyledOption>
+                ))
+            }
+
+
         </StyledWrapper>
     )
 }
--- src/Views/Main/style.tsx
+++ src/Views/Main/style.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const StyledWrapper = styled.section`
     padding-top:7rem;
     display:flex;
+    min-height:100vh;
     flex-direction:column;
     justify-content:space-around;
     align-items:center;
@@ -15,4 +16,15 @@ export const StyledImage = styled.figure`
         width:100%;
         height:100%;
     }
+`
+
+export const StyledOption = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
+
+export const StyledName = styled.span`
+    color:${({ theme }) => theme.colors.primary};
+    font-size:${({ theme }) => theme.fontSizes.medium}
 `
