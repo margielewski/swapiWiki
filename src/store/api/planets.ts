689be452b36fdde1cc46248e60ce2d31d1db3701
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-10-24 16:39:13 +0200

    .

--- src/store/api/planets.ts
+++ src/store/api/planets.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://swapi.dev/api/'
+const API_URL = 'http://swapi.dev/api/'
 
 export const GETPlanets = (postfix: string) => {
     return axios.get(`${API_URL}planets${postfix}`)
