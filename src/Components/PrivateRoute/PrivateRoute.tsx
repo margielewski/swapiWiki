a2bca988e3604a2beda379bc88e0b891f2010336
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-10-29 13:08:06 +0100

    fix router warning

--- src/Components/PrivateRoute/PrivateRoute.tsx
+++ src/Components/PrivateRoute/PrivateRoute.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Redirect, Route } from 'react-router-dom';
 
 export default function PrivateRoute({ component: Component, auth, ...children }: any) {
-
     if (auth.loggedIn === true) {
-        return <Route {...children} render={props => <Component {...props} />} />;
+        return <Route {...children} />;
     }
     return <Redirect to="/login" />;
 }
