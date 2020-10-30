ff973ffc1ba1cb419444cb240f64b7a41737a786
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-10-30 14:31:54 +0100

    types

--- src/store/helpers/getDetails.ts
+++ src/store/helpers/getDetails.ts
@@ -1,8 +1,18 @@
-import { Dispatch } from 'redux';
 import axios from 'axios';
+import { Action } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { RootStore } from '../store';
 
-export function getDetails(url: string, successAction: any, errorAction: any) {
-    return async (dispatch: Dispatch) => {
+import { StarshipDetailsActions } from './../starships/starships.types';
+import { PlanetDetailsActions } from './../planets/planets.types';
+import { CharacterDetailsActions } from './../characters/characters.types';
+
+type ActionSuccessType<T> = (data: T) => CharacterDetailsActions | PlanetDetailsActions | StarshipDetailsActions
+
+type ActionErrorType = (message: string) => CharacterDetailsActions | PlanetDetailsActions | StarshipDetailsActions
+
+export function getDetails<T>(url: string, successAction: ActionSuccessType<T>, errorAction: ActionErrorType) {
+    return async (dispatch: ThunkDispatch<RootStore, void, Action>) => {
         try {
             const response = await axios.get(url)
             const { data } = response;
